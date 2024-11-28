27c2a254b6c7bbf6408bcdb2f139980a868734c2
Jailton <junio.jr555@hotmail.com>
2024-11-28 17:10:41 -0300

    CRUD e ORMFUNCIONANDO

--- server.js
+++ server.js
@@ -7,25 +7,52 @@ const prisma = new PrismaClient()
 const app = express()
 app.use(express.json())
 
-const users = []
 
-app.get('/usuarios', (req, res) => {
+
+app.get('/usuarios', async (req, res) => {
+    const users = await prisma.user.findMany()
 
     res.status(200).json(users)
 })
 
 app.post('/usuarios', async (req, res) => {
 
-   await prisma.user.create({
-        data:{
+    const user = await prisma.user.create({
+        data: {
             email: req.body.email,
             name: req.body.name,
-            age:req.body.age
-            
+            age: req.body.age
+
         }
     })
 
-    res.status(201).json({ mensagem: "Usuario criado com sucesso" })
+    res.status(201).json(user)
+})
+
+app.put('/usuarios/:id', async (req, res) => {
+
+    const user = await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+
+        }
+    })
+
+    res.status(200).json(user)
+})
+
+app.delete('/usuarios/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+    res.status(200).json({ message: "Usuario deletado com sucesso!" })
 })
 
 
